f28444c32f9df11c73c6cc1cc067b0ae7602cad8
Asif-102 <asif1zero2@gmail.com>
2025-01-15 22:13:56 +0600

    test with playwright

---
+++ app/blog/[slug]/page.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+
+import BlogPage from "./page";
+
+test("Test the blog dynamic route", () => {
+  render(<BlogPage params={{ slug: "Test" }} />);
+
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Slug: Test" })
+  ).toBeDefined();
+});
